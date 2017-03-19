8310efb6cdf4c2a1d9dacc337d7ea78a557537e3
Chris Godber <chrispgodber@gmail.com>
2017-03-19 22:26:39 +0000

    Babel Install

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ import content from './content';
 
 export default class IndexPage extends React.Component {
   render() {
+            <h1>Render Test</h1>
    // return (
-      <div className="home">
-        <div className="athletes-selector">
+      //<div className="home">
+      //  <div className="main">
           //{athletes.map(athleteData => <AthletePreview key={athleteData.id} {...athleteData} />)}
         </div>
       </div>
